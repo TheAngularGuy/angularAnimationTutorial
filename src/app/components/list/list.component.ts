3cb82008ed129946c1054000b91fe266c8e5e8f3
ultraMIND <aouas.m@gmail.com>
2019-08-24 21:13:38 +0200

    ajustment

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -29,29 +29,29 @@ export class ListComponent {
   contactList: Contact[] = CONTACTS_MOCK;
   selectedContact: Contact;
 
-  onSelectItem(c: Contact) {
+  onSelectItem(contact: Contact) {
     // * selecting a contact to focus on
-    this.selectedContact = c;
+    this.selectedContact = contact ? { ...contact } : null;
   }
 
   onAddItem() {
-    const rndNum = ~~(Math.random() * 100);
-    const c: Contact = {
+    const rndNum = Date.now();
+    const newContact: Contact = {
       id: this.contactList.length * rndNum,
       name: `Contact ${this.contactList.length}`,
       email: `email${this.contactList.length}@provider.com`,
       avatarUrl: `https://api.adorable.io/avatars/285/${rndNum}`,
     };
     // * adding a new contact to the list
-    this.contactList = [c, ...this.contactList];
+    this.contactList = [newContact, ...this.contactList];
     // * selecting the newly created contact
-    this.onSelectItem(c);
+    this.onSelectItem(newContact);
   }
 
-  onDeleteItem(c: Contact) {
+  onDeleteItem(contact: Contact) {
     // * removing a contact from the list
-    this.contactList = this.contactList.filter(v => v.id != c.id);
-    if (this.selectedContact && this.selectedContact.id == c.id) {
+    this.contactList = this.contactList.filter(c => c.id != contact.id);
+    if (this.selectedContact && this.selectedContact.id == contact.id) {
       // * if the removed contact is beaing focused on, then we remove the focus
       this.onSelectItem(null);
     }
