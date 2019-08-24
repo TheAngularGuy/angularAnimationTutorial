9da64236c8cffbe74a70ab2147c91caf0ba4f826
ultraMIND <aouas.m@gmail.com>
2019-08-24 20:41:57 +0200

    step 2 finished

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Contact {
+  id: number;
+  name: string;
+  email: string;
+  avatarUrl: string;
+}
+
+const CONTACTS_MOCK: Contact[] = new Array(5)
+  .fill({})
+  .map(
+    (c: Contact, i: number) =>
+      ({
+        id: i,
+        name: `Contact ${i}`,
+        email: `email${i}@provider.com`,
+        avatarUrl: `https://api.adorable.io/avatars/100/${~~(Math.random() * 100)}`,
+      } as Contact),
+  )
+  .reverse(); // * to have them sorted in DESC order
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
+  contactList: Contact[] = CONTACTS_MOCK;
+  selectedContact: Contact;
 
-  constructor() { }
-
-  ngOnInit() {
+  onSelectItem(c: Contact) {
+    // * selecting a contact to focus on
+    this.selectedContact = c;
   }
 
+  onAddItem() {
+    const rndNum = ~~(Math.random() * 100);
+    const c: Contact = {
+      id: this.contactList.length * rndNum,
+      name: `Contact ${this.contactList.length}`,
+      email: `email${this.contactList.length}@provider.com`,
+      avatarUrl: `https://api.adorable.io/avatars/285/${rndNum}`,
+    };
+    // * adding a new contact to the list
+    this.contactList = [c, ...this.contactList];
+    // * selecting the newly created contact
+    this.onSelectItem(c);
+  }
+
+  onDeleteItem(c: Contact) {
+    // * removing a contact from the list
+    this.contactList = this.contactList.filter(v => v.id != c.id);
+    if (this.selectedContact && this.selectedContact.id == c.id) {
+      // * if the removed contact is beaing focused on, then we remove the focus
+      this.onSelectItem(null);
+    }
+  }
 }
